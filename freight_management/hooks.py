0ff4283b485df7f4bd6f0b96411efe14d50e8f0c
rogerboy38 <rogerboy38@hotmail.com>
2026-01-22 08:46:25 -0600

    Add freight management updates

---
+++ freight_management/hooks.py
@@ -0,0 +1,16 @@
+# Temporary fix for __version__ import issue
+try:
+    from . import __version__ as app_version
+except ImportError:
+    # If import fails, define it here
+    __version__ = "0.0.1"
+    app_version = __version__
+
+app_name = "freight_management"
+app_title = "Freight Management"
+app_publisher = "Your Name"
+app_description = "Freight Management"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "your@email.com"
+app_license = "MIT"
